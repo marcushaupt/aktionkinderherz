02189fc7e13614ea29600cad7f9786508f34c60b
Marcus Haupt <taros-sonnenbad07@icloud.com>
2024-01-06 12:51:22 +0100

    clean up project files

--- _old/mambots/editors/jce/jscripts/tiny_mce/plugins/directionality/langs/de.js
+++
@@ -1,6 +0,0 @@
-// UK lang variables
-
-tinyMCE.addToLang('',{
-directionality_ltr_desc : 'Sprachrichtung von Links nach Rechts',
-directionality_rtl_desc : 'Sprachrichtung von Rechts nach Links'
-});
--- _old/mambots/editors/jce/jscripts/tiny_mce/plugins/fullscreen/langs/de.js
+++
@@ -1,6 +0,0 @@
-// UK lang variables
-
-tinyMCE.addToLang('',{
-fullscreen_title : 'Vollbild Modus',
-fullscreen_desc : 'Im Vollbild Modus anzeigen'
-});
